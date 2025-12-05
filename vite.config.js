782039394df46d5c0e64f0b859037b99257e93e8
guangfei.zhao <guangfei.zhao@t-systems.com>
2025-12-05 16:26:00 +0800

    add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { fileURLToPath, URL } from 'node:url'
@@ -12,7 +13,8 @@ export default defineConfig({
     }
   },
   plugins: [
-    vue(), 
+    vue(),
+    tailwindcss(),
     Components({
       resolvers: [
         AntDesignVueResolver({ importStyle: false })
